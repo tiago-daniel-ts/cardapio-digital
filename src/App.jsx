2f1737cba4a93bf24c86d998efdb9e7fdd8db0e3
tiago-daniel-ts <157622559+tiago-daniel-ts@users.noreply.github.com>
2024-01-31 22:22:47 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Hashtaurante from "./assets/hashtaurante.webp";
+import "./App.css";
+import { Navegacao } from "./Navegacao";
+import { ItemCardapio } from "./ItemCardapio";
+import { pratosPrincipais, sobremesas, bebidas } from "./cardapio";
+
+
+export function App() {
+  const paginasMenu = [pratosPrincipais, sobremesas, bebidas];
+  const [paginaSelecionada, atualizarPaginaSelecionada] = useState(0);
+
+  return <>
+          <img src={Hashtaurante} alt="" className="capa"/>
+          <Navegacao atualizarPaginaSelecionada={atualizarPaginaSelecionada}/>
+          <div className="menu">
+            {paginasMenu[paginaSelecionada].map(item => <ItemCardapio name={item.nome} descricao={item.descricao} preco={item.preco} imagem={item.imagem}/>)}
+          </div>
+        </>
+}
